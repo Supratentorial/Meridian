a68b22d193e64df3d60f6fe6c19fca8d4b37a5ce
Supratentorial <blake.mumford@indoctrinate.com.au>
2015-05-02 16:16:06 +0900

    Initial commit
    Begun work on gulpfile.js.
    Added gulp task for SCSS transpilation, minification.

---
+++ WebApp/src/Gulpfile.js
@@ -0,0 +1,38 @@
+﻿
+var gulp = require("gulp");
+var rimraf = require("rimraf");
+var fs = require("fs");
+var watch = require("gulp-watch");
+var rename = require("gulp-rename");
+var minifycss = require("gulp-minify-css");
+var sass = require("gulp-sass");
+
+eval("var project = " + fs.readFileSync("./project.json"));
+
+var paths = {
+    lib: "./" + project.webroot + "/lib/",
+    dist: "./" + project.webroot + "/styles",
+    bower: "./bower_components/",
+    styles: "./Styles/**/*.scss",
+    scripts: "./Scripts/**/*.ts"
+};
+
+gulp.task("clean", function (cb) {
+    rimraf(paths.lib, cb);
+});
+
+//Takes SCSS files, transpiles to both uniminified and minified CSS, adding them to dist.
+gulp.task("styles", function () {
+    gulp.src(paths.styles)
+    .pipe(sass({ style: "expanded" }))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(minifycss())
+    .pipe(gulp.dest(paths.dist));
+});
+
+
+gulp.task("default", ["clean"], function () {
+    gulp.watch(paths.styles, ["styles"]);
+    gulp.watch(paths.scripts, ["scripts"]);
+});
---
+++ WebApp/src/Models/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Meridian.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public List<string> MiddleNames { get; set; }
+
+    }
+}
